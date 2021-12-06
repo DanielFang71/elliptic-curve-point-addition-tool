f6e2cac46b87bf962776c4a5c5fe860165c5f898
Ruikai Fang <ruikai.fang@mail.utoronto.ca>
2021-12-06 23:27:22 +0800

    Add files via upload

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 from collections import namedtuple
-
+from tabulate import tabulate
 Point = namedtuple("Point", "x y")
 
 # The point at infinity (origin for the group law).
-O = 'Origin'
+O = 'Point(x=inf, y=inf)'
 # Choose a particular curve and prime.  We assume that p > 3.
 # elliptic curve y^2 = x^3 + ax + b
-p = 557
-a = -10
-b = 21
+p = 5
+a = 1
+b = 1
 
 
 def valid(P):
@@ -85,21 +85,76 @@ def compute_n_P(P, n):
     return "compute" + str(n) + "P=", temp
 
 
-m = 7
-P = Point(2, 3)
-res = []
-for k in range(-7, 8):
-    for j in range(0, 8):
-        n1 = 558 + 2 * k * m + j
-        n2 = 558 + 2 * k * m - j
-        if compute_n_P(P, n1)[1] == O:
-            res.append(["n1", n1, k, j])
-        if compute_n_P(P, n2)[1] == O:
-            res.append(["n2", n2, k, j])
-print(res)
+def compute_all_valid_points():
+    # print(O)
+    res = []
+    for x in range(p):
+        for y in range(p):
+            if valid(Point(x, y)):
+                # print((x, y))
+                res.append(Point(x, y))
+    return res
 
-print(compute_n_P(P, 567//3))
-print(compute_n_P(P, 567//7))
 
-print(compute_n_P(P, 189//3))
-print(compute_n_P(P, 189//7))
+# res = compute_all_valid_points()
+# for item in res:
+#     print("Computation for point", item)
+#     i = 2
+#     count = compute_n_P(item, i)
+#     print(count)
+#     while count[1] != O:
+#         i += 1
+#         count = compute_n_P(item, i)
+#         print(count)
+#     print("i=", i, item, compute_n_P(item, i))
+#     print()
+
+
+# res = compute_all_valid_points()
+# res.append(O)
+# table = []
+#
+# table.append(["Addition Table"] + res)
+# temp = ["p+e"]
+# for i1 in res:
+#     count = ec_add(i1, O)
+#     temp.append(count)
+# table.append(temp)
+# temp = ["e+p"]
+# for i1 in res:
+#     count = ec_add(O, i1)
+#     temp.append(count)
+# table.append(temp)
+# print(tabulate(table, tablefmt='fancy_grid'))
+
+# table.append(["Addition Table"] + res)
+# for i1 in res:
+#     temp = [i1]
+#     for i2 in res:
+#         count = ec_add(i1, i2)
+#         temp.append(count)
+#         # print("Add", i1, i2, "=",count)
+#     table.append(temp)
+# print(tabulate(table, tablefmt='fancy_grid'))
+
+
+# Steps for find order of Point P(2,3)
+
+# m = 7
+# P = Point(2, 3)
+# res = []
+# for k in range(-7, 8):
+#     for j in range(0, 8):
+#         n1 = 558 + 2 * k * m + j
+#         n2 = 558 + 2 * k * m - j
+#         if compute_n_P(P, n1)[1] == O:
+#             res.append(["n1", n1, k, j])
+#         if compute_n_P(P, n2)[1] == O:
+#             res.append(["n2", n2, k, j])
+# print(res)
+#
+# print(compute_n_P(P, 567//3))
+# print(compute_n_P(P, 567//7))
+#
+# print(compute_n_P(P, 189//3))
+# print(compute_n_P(P, 189//7))
